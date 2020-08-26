4e5b0cac1a7adb6bc1f0946979008fe1e80e5371
Alex Vinicius Ribeiro Rodrigues <47673758+AlexRodriguesbeta@users.noreply.github.com>
2020-08-26 20:31:06 -0300

    pwa - captura câmera e microfone

---
+++ js/main.js
@@ -0,0 +1,52 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
+
+function getUserMedia(constraints) {
+    if (navigator.mediaDevices) {
+      return navigator.mediaDevices.getUserMedia(constraints);
+    }
+      
+    var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia;
+      
+    if (legacyApi) {
+      return new Promise(function (resolve, reject) {
+        legacyApi.bind(navigator)(constraints, resolve, reject);
+      });
+    }
+  }
+  
+  function getStream (type) {
+    if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+      !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+      alert('User Media API not supported.');
+      return;
+    }
+  
+    var constraints = {};
+    constraints[type] = true;
+    
+    getUserMedia(constraints)
+      .then(function (stream) {
+        var mediaControl = document.querySelector(type);
+        
+        if ('srcObject' in mediaControl) {
+          mediaControl.srcObject = stream;
+        } else if (navigator.mozGetUserMedia) {
+          mediaControl.mozSrcObject = stream;
+        } else {
+          mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+        }
+        
+        mediaControl.play();
+      })
+      .catch(function (err) {
+        alert('Error: ' + err);
+      });
+    }
